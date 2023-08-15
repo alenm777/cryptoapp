65e7d48a1c14dc54876ccd99ceceb922167f5073
Alén <alenm.1717@gmail.com>
2023-08-15 19:29:32 -0300

    primer cambio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar, Exchanges, HomePage, Cryptocurrencies, News, CryptoDetails } from "./componentes";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return (
+   <div classname="app">
+    <div className="navbar">
+<Navbar />
     </div>
-  );
-}
+    <div className="main">
+<Layout>
+    <div className="routes">
+    <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="news" element={<News />} />
+            </Routes>      
+    </div>
+</Layout>
+    </div>
+    <div className="footer">
+    <Typography.Title level={5} style={{color: "white", textAlign: "center" }}>
+        Crypto X <br />
+        All rights reserverd
+    </Typography.Title>
+    <Space>
+        <Link to="/">Home</Link>
+        <Link to="/exchanges">Exchanges</Link>
+        <Link to="/news">News</Link>
+    </Space>
+    </div>
+    </div>
+ );
+};
 
-export default App;
+export default App
---
+++ src/componentes/Cryptocurrencies.jsx
@@ -0,0 +1,49 @@
+import React, {useState, useEffect } from 'react';
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+const [SearchTerm, setSearchTerm] = useState("");
+
+useEffect(() => {
+  setCryptos(cryptosList?.data?.coins);
+  const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm))
+
+  setCryptos(filteredData);
+}, [cryptosList, SearchTerm]);
+
+if(isFetching) return "loading ...";
+
+  return (
+    <>
+    <div classname="search-crypto">
+<Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.vulue)} />
+    </div>
+    <Row gutters={[32, 32]} className='crypto-card-container'>
+{cryptos?.map((currency) => (
+  <Col xs={24} sm={12} lg={6} classname="crypto-card" key={currency.id}>
+<Link to={`/crypto/${currency.id}`}>
+  <Card
+  title={`${currency.rank}. ${currency.name}`}
+  extra={<img className='crypto-image' src={currency.iconUrl} style={{ maxWidth: '35px' }}/>} 
+  hoverable
+  >
+<p>Price: {millify(currency.price)}</p>
+<p>Market Cap: {millify(currency.marketCap)}</p>
+<p>Daily Change: {millify(currency.change)}%</p>
+  </Card>
+</Link>
+  </Col>
+))}
+    </Row>
+    </>
+  )
+}
+
+export default Cryptocurrencies
